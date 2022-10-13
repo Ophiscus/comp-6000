2529d7bdaecd63f2ad21996efb69cb2570e52b69
Kabeerat Adeyinka <ka459@kent.ac.uk>
2022-10-13 18:09:11 +0000

    First draft of login page

---
+++ bcit-ci-CodeIgniter-3.1.13-0-gbcb17eb/bcit-ci-CodeIgniter-bcb17eb/application/views/login_page.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "utf-8">
+	<title>Company name</title>
+	
+	<div class ="container">
+	    <h1>Login</h1>
+		<label for = "usern"><b>Username</b></label>
+		<input type = "text" placeholder = "Enter username" name = "usern" required>
+		
+		<label for = "ps"><b>Password</b></label>
+		<input type = "text" placeholde = "Enter Password" name="ps" required>
+		
+		<button type="submit">login</button>
+	</div>	
+		
