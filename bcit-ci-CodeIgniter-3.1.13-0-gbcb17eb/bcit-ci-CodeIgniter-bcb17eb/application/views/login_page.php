d9af9bd3001d7bb2c0d058ad5c2e6bcb80682579
Kabeerat Adeyinka <ka459@kent.ac.uk>
2022-10-17 14:45:03 +0000

    Replace login_page.php

--- bcit-ci-CodeIgniter-3.1.13-0-gbcb17eb/bcit-ci-CodeIgniter-bcb17eb/application/views/login_page.php
+++ bcit-ci-CodeIgniter-3.1.13-0-gbcb17eb/bcit-ci-CodeIgniter-bcb17eb/application/views/login_page.php
@@ -1,19 +1,57 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang = "en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset = "utf-8">
-	<title>Company name</title>
+<style>
+
+
+body{
+	background-color: green;
+}
+
+ 
+
+h1{
+	colour: black;
+	text-align: left;
+}
+
+.container{
+background-color: lightgrey;
+width: 300 px;
+border: 3px solid #f1f1f1;
+padding: 50px;
+margin:20px;
+height: 20px;
+
+}
+
+</style>
+</head>
+<body>
+
+<!-- Heading of the page  -->
+
+	<h1>Company name</h1>
 	
+<!-- Form to enter in Username and Password   -->
 	<div class ="container">
 	    <h1>Login</h1>
 		<label for = "usern"><b>Username</b></label>
 		<input type = "text" placeholder = "Enter username" name = "usern" required>
 		
 		<label for = "ps"><b>Password</b></label>
-		<input type = "text" placeholde = "Enter Password" name="ps" required>
+		<input type = "text" placeholder = "Enter Password" name="ps" required>
 		
+<!-- Submit button // need to map the submit button to the dashboard -->
 		<button type="submit">login</button>
-	</div>	
-		
+
+<!-- when one or both fields don't have a value to check -->
+...
+
+<!-- when incorrect details that don't match on the database -->
+...
+
+
+</div>
+</body>
+</html>
