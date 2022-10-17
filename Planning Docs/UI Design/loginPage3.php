f0ecad152b110f5901c13ab720c91d098f8e4cc9
Daniel Essex <de224@kent.ac.uk>
2022-10-17 09:15:55 +0000

    Login Page

---
+++ Planning Docs/UI Design/loginPage3.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+
+body{
+	background-color: green;
+}
+
+ 
+
+h1{
+	colour: black;
+	text-align: left;
+}
+
+.container{
+background-color: lightgrey;
+width: 300 px;
+border : 15px solid green;
+padding: 50px;
+margin:20px;
+height: 20px;
+
+}
+
+</style>
+</head>
+<body>
+
+<!-- Heading of the page  -->
+
+	<h1>Company name</h1>
+	
+<!-- Form to enter in Username and Password   -->
+	<div class ="container">
+	    <h1>Login</h1>
+		<label for = "usern"><b>Username</b></label>
+		<input type = "text" placeholder = "Enter username" name = "usern" required>
+		
+		<label for = "ps"><b>Password</b></label>
+		<input type = "text" placeholder = "Enter Password" name="ps" required>
+		
+<!-- Submit button // need to map the submit button to the dashboard -->
+		<button type="submit">login</button>
+
+<!-- when one or both fields don't have a value to check -->
+...
+
+<!-- when incorrect details that don't match on the database -->
+...
+
+
+</div>
+</body>
+</html>
