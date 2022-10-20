55610ec19f63c8a957cc5cb3af50d74cbcbd0672
Kabeerat Adeyinka <ka459@kent.ac.uk>
2022-10-20 11:46:55 +0000

    Update base URL to local folder

--- bcit-ci-CodeIgniter-3.1.13-0-gbcb17eb/bcit-ci-CodeIgniter-bcb17eb/application/config/config.php
+++ bcit-ci-CodeIgniter-3.1.13-0-gbcb17eb/bcit-ci-CodeIgniter-bcb17eb/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https:\\raptor.kent.ac.uk\exports\proj\comp6000\project\14';
 
 /*
 |--------------------------------------------------------------------------
