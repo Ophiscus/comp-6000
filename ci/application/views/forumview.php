d93e3d61127ce187d16a52c432c2d0762adc67f7
Kieran Cole <kc576@kent.ac.uk>
2022-10-25 18:02:27 +0100

    Added navigation html fragment

--- ci/application/views/forumview.php
+++ ci/application/views/forumview.php
@@ -12,23 +12,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body>
 
-<div id="main">
-	<div class="topnav">
-	<button class="openbtn" onclick="openNav()">☰</button>
-	<a class="active" href="#home">Home</a>
-	<a href="#Announcements">Announcements</a>
-	<a href="#contact">Contact Us</a>
-	<a href="#about">About</a>
-	<a href="#login">Login</a>
-	</div>
+<div id="main" include-html="nav.html">
 
-	<div id="mySidebar" class="sidebar">
-		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">X</a>
-		<a href="#">Maneger</a>
-		<a href="#">Statistic</a>
-		<a href="#">Calendar</a>
-		<a href="#">Contact Us</a>
-	</div>
 </div>
 
 <div id="content">
