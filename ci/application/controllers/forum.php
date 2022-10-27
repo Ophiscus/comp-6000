161b242b53a70eb44192da62e958df3b20ce3455
Kieran Cole <kc576@kent.ac.uk>
2022-10-27 12:56:34 +0100

    Some small changes to forum functionality. Moved all shared view scripts to an assets folder

--- ci/application/controllers/forum.php
+++ ci/application/controllers/forum.php
@@ -3,6 +3,8 @@
 class forum extends CI_Controller {
 	$forum = new controller;
 	
+	$this->load->view('forumview');
+	
 	public function getMessages($user)
 	{
 		$sql = "SELECT user, post_message FROM Posts ORDER BY date";
--- ci/application/views/forumview.php
+++ ci/application/views/forumview.php
@@ -5,25 +5,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="stylesheet" href="forum_style.css">
-<link rel="stylesheet" href="nav_style.css">
+<link rel="stylesheet" href="assets/forum_style.css">
+<link rel="stylesheet" href="assets/nav_style.css">
 
 </head>
 
 <body>
 
-<div id="main" include-html="nav.html">
+<div id="main" type="html" source="assets/nav.html">
 
 </div>
 
-<div id="content">
+<!--<div id="content">
 	<div id="info_box"><?php ["message_count"]?></div>
 	<div class="message_template">
 		<div class="name_container"><?php ["post_name"]?></div>
 		<div class="title_container"><?php ["post_title"]?></div>
 		<div class="message_container"><?php ["post_message"]?></div>
 	</div>
-</div>
+</div>-->
+
+<?php
+	   while($row = mysql_fetch_array($query)) {?>
+		   <tr>
+			<td><?php echo $row[''];?></td>
+		   </tr>
+		   <tr>
+			<td><?php echo $row[''];?></td>
+		   </tr>
+		   <tr>
+			<td><?php echo $row[''];?></td>
+		   </tr>   
+<?php } ?>
 
 </body>
 
