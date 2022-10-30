75e5c403995f4b3d8e3a41093a0307c5d7995d35
Kieran Cole <kc576@kent.ac.uk>
2022-10-30 14:54:16 +0000

    Updated table layout for forum messages, added corresponding CSS in assets folder

--- ci/application/views/forumview.php
+++ ci/application/views/forumview.php
@@ -14,36 +14,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="main" type="html" source="assets/nav.html"></div>
 
-<!--Code for example html-->
-<!--<div id="content">
-	<div id="info_box">/div>
-	<div class="message_template">
-		<div class="name_container">Name</div>
-		<div class="title_container">Title</div>
-		<div class="message_container">Message</div>
-	</div>
-</div>-->
-
-<table border = "1">
+<table border = "1" id="message_container">
 <thead>
-<tr>
-	<td>Subject</td>
-	<td>Content</td>
-<tr>
 </thead>
 <tbody>
 <?php
 
 foreach ($results as $row) {
 	?>
-	<tr>
-		<td>
-			<?php echo $row['Subject']?>
-		</td>
-		<td>
-			<?php echo $row['Content']?>
-		</td>
-	</tr>
+    <tr class="post_head">
+      <td class="subject">
+        <?php echo $row['Subject']?>
+      </td>
+    </tr>
+    <tr class="message">
+      <td>
+        <?php echo $row['Content']?>
+      </td>
+    </tr>
 	<?php
    }   
 ?>
