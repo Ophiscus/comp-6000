f47cac9c7d85b49857edf1fe4aa5bce5c269e807
Kabeerat Adeyinka <ka459@kent.ac.uk>
2022-11-17 12:28:28 +0000

    Replace login.php

--- ci/application/controllers/login.php
+++ ci/application/controllers/login.php
@@ -1,46 +1,48 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
-{public function index()
+class Login extends CI_Controller
+{
+	public function __construct()
 	{
-		//redirect(base_url().'User/View/'.$this->session->userdata('username'));
-		$this->load->view('loginview');
+		parent::__construct();
+		$this->load->library('form_validation'); 											//this loads the form validation function
+		$this->load->library('session');
+		$this->load->helper('url');
 	}
 
+	public function index()
+	{
+		//redirect(base_url().'User/View/'.$this->session->userdata('username'));
+		
+	}
+	
+	public function showView()
+	{
+		$this->load->view('loginview');
+	}
+	
 	function dologin()
 	{
 		$username = $this->input->post('username');
 		$_SESSION['username'] = $username;
 
 		$this->load->model('Login_Model');
-		$user_exists = $this->User_Model->checkLogin(
-			$this->input->post('username'),													//collect username from form and agaist with database
+		$user_exists = $this->Login_Model->checkLogin(
+			$this->input->post('username'),													//collect //username from form and agaist with database
 			$this->input->post('password')
 		);
 
 		if ($user_exists) {
 			
-			$this->load->view('Forumview');
+			$this->load->view('forumview');                                               
 		}
 		else {
 			//session_destroy();
 			$this->session->set_flashdata('error', 'Invalid Username or Password');  		//When the user login fails an error message display and redirect to the login form.
-			redirect('index.php/User/login');
+			redirect('/Login/showView','refresh');
 		}
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+?>
