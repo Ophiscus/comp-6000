3b0599e4917d953fa08d08c77c5965d9055ee35f
Kieran Cole <kc576@kent.ac.uk>
2022-11-17 11:29:28 +0000

    Finished employee side of the forum, updated navigation links in nav bar

--- ci/assets/nav.js
+++ ci/assets/nav.js
@@ -1,12 +1,9 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  //Content window for all main pages
-  document.getElementById("content").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-  document.getElementById("content").style.marginLeft = "0";
 }
