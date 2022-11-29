64ae96b45e1348f375163a1f8eb7311cf2177552
Daniel Essex <de224@kent.ac.uk>
2022-11-29 16:53:11 +0000

    Commenting and resolved formatting errors

--- ci/application/models/Signup_Model.php
+++ ci/application/models/Signup_Model.php
@@ -3,10 +3,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Signup_Model extends CI_Model
 {
+    // need to make sure username is unique when the database is being checked.
     public function addUser($username,$password)
     {
+        //security feature for passwords to be encrypted
        $password = sha1($password);
        $sql = "INSERT INTO Staff(username,password,email) VALUES($username,$password,NULL,NULL)";
        $query = $this ->db->query($sql);
     }
 }
+?>
