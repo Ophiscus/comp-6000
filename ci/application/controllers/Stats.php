e39cd7d028910e970724f1e856c65f43f9115435
Erol <ee285@lent.ac.uk>
2023-01-05 13:36:25 +0000

    Added Session checks for the user being logged in and authorised (as a manager).

--- ci/application/controllers/Stats.php
+++ ci/application/controllers/Stats.php
@@ -2,32 +2,42 @@
 defined('BASEPATH') OR exit('No direct script access allowed'); 
 class Stats extends CI_Controller {
 
+    public function __construct(){
+
+        parent::__construct();
+
+        //authorisation check
+        $this->load->helper('url');
+        $this->load->library('session');
+
+        if (isset($_SESSION['role']) && $_SESSION['role'] === "Manager") {
+
+        } else {
+          redirect(base_url());
+        }
+    }
+    
     public function index(){
        
         
     }
 
     public function show() {
-        $this->output->enable_profiler(TRUE);
-        //adding helper because the view calls base_url.
-        $this->load->helper('url');
-        $this->load->model('Stats_model');
+            $this->load->model('Stats_model');
 
-        $stock = $this->Stats_model->getStock();
-        $income = $this->Stats_model->getIncome();
-        $expense = $this->Stats_model->getExpenses();
+            $stock = $this->Stats_model->getStock();
+            $income = $this->Stats_model->getIncome();
+            $expense = $this->Stats_model->getExpenses();
 
-        //$stockData = array("stockData" => $stock);
-        //$profitData = array("profitData" => $profit);
+            //$stockData = array("stockData" => $stock);
+            //$profitData = array("profitData" => $profit);
 
-        $data = array("stockData" => $stock, "incomeData" => $income, "expensesData" => $expense);
+            $data = array("stockData" => $stock, "incomeData" => $income, "expensesData" => $expense);
 
-        $this->load->view('stats',$data);
+            $this->load->view('stats', $data);
     }
 
     function postStock() {
-        //once sessions are working, check that the user is logged in and a manager
-        if(true) {
         //grab the data from the form on the view
         $name = $this->input->post("ItemName");
         $cost = $this->input->post("ItemCost");
@@ -40,10 +50,6 @@ class Stats extends CI_Controller {
         echo($id);
         //redirect to stats view page
         //redirect("stats/show");
-        } else {
-            //handling for non-logged in users.
-            $this->load->view('login');
-        }
     }
 
     function postIncome(){
