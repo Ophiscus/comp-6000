ab4a6b6a70a87004338faeb8f19fb90a323e3ad6
Kabeerat Adeyinka <ka459@kent.ac.uk>
2023-02-02 15:42:09 +0000

    Hr resources view file

---
+++ ci/application/views/resourceview.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+
+<style>
+body{
+background-image: url("youngchef.jpg");
+background-size: 500px;
+background-color: #FFFDD0;
+}
+table,tr,td{
+    border : 1px solid;
+    width : 100%;
+    border-collapse:collapse;
+}
+</style>
+
+<body onload="checkManagerElements('<?php echo $this->session->userdata('role') ?>')">
+</head>
+<body>
+<div id="manage_tools" class="manager">
+	<div id="tools" onload="isManager(this.id, '<?php echo $this->session->userdata('role') ?>')">
+	<?php echo $error;?>
+
+<?php echo form_open_multipart('Resources/do_upload');?>
+
+<h1 style="font-family:verdana";>Resources</h1>
+
+<input type="file" name="userfile" size="20" />
+
+<br /><br />
+
+<input type="submit" value="Add file" />
+
+<?php echo "</form>"?>
+		
+</div>
+
+
+
+<table>
+    <tr>
+<td>Resource Type</td>
+<td>Documents</td>
+</tr>
+<tr>
+<td>Training</td>
+</tr>
+</table>    
+
+</body>
+</html>
