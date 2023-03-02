81c4b10688a38e4463880bc39664c85003467b99
Kabeerat Adeyinka <ka459@kent.ac.uk>
2023-03-02 12:26:43 +0000

    view created to showcase videos in a table format

---
+++ ci/application/views/videoview.php
@@ -0,0 +1,43 @@
+<html>
+  <head>
+  <style>
+body{
+background-image: url("youngchef.jpg");
+background-size: 500px;
+background-color: #FFFDD0;
+}
+
+table{
+  
+}
+</style>
+
+    <title>Video Player</title>
+  </head>
+  <body>
+  <h1 style="font-family:verdana";>Training Videos</h1>
+
+    <!--
+      <video controls>
+        <source src="" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    -->
+    <table>
+    <thead>
+        <tr>
+            <th>Title</th>
+            <th>Video</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($video as $v): ?>
+        <tr>
+            <td><?php echo $v->Title;?></td>
+            <td><video src="<?php echo $v->location;?>" controls></video></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+  </body>
+</html>
